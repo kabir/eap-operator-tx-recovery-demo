8b07202f84ee9b9618fbd98c201c6b6dda0b0286
Kabir Khan <kkhan@redhat.com>
2023-05-03 17:49:29 +0100

    Delete output

--- src/main/scripts/s2i/install.sh
+++ src/main/scripts/s2i/install.sh
@@ -13,18 +13,13 @@ unzip byteman.zip
 mv byteman-download-4.0.21 $JBOSS_HOME/extensions/byteman
 chmod -R o-rwx $JBOSS_HOME/extensions/byteman
 
-echo "Checking if there is a standalone.conf file."
-cat $JBOSS_HOME/bin/standalone.conf
-echo "Checked for standalone.conf file."
-
-
 echo "export BYTEMAN_HOME=\"$JBOSS_HOME/extensions/byteman\""  >> $JBOSS_HOME/bin/standalone.conf
 # -Xverify:none is deprecated from Java 13, but I don't see another way right now
 # to follow the recommendations in https://www.baeldung.com/java-lang-verifyerror
 echo "JAVA_OPTS=\"-Xverify:none -javaagent:\${BYTEMAN_HOME}/lib/byteman.jar=boot:\${BYTEMAN_HOME}/lib/byteman.jar,listener:true \${JAVA_OPTS}\"" >> $JBOSS_HOME/bin/standalone.conf
 
 
-echo "Double checking standalone.conf"
-tail $JBOSS_HOME/bin/standalone.conf
-echo "Double-checked standalone.conf file."
+#echo "Double checking standalone.conf"
+#tail $JBOSS_HOME/bin/standalone.conf
+#echo "Double-checked standalone.conf file."
 
