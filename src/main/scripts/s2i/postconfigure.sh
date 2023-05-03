687068285ef088c3d8dab5680fcc5ef438c7bfe9
Kabir Khan <kkhan@redhat.com>
2023-05-03 14:42:45 +0100

    Fixes to the postconfigure.sh and cli scripts

--- src/main/scripts/s2i/postconfigure.sh
+++ src/main/scripts/s2i/postconfigure.sh
@@ -5,8 +5,12 @@
 # This script is expected to be copied to
 # $JBOSS_HOME/extensions/ folder by script install.sh
 
-echo "Configuring server with CLI"
+
+echo "Configuring server ${JBOSS_HOME}/extensions/initialize-server.cli"
+
+POSTCONFIGURE_PROPERTIES_FILE="${JBOSS_HOME}/extensions/cli.openshift.properties"
+
 [ "x$SCRIPT_DEBUG" = "xtrue" ] && cat "${JBOSS_HOME}/extensions/initialize-server.cli"
-"${JBOSS_HOME}"/bin/jboss-cli.sh \
---file="${JBOSS_HOME}/extensions/initialize-server.cli" \
---properties="${POSTCONFIGURE_PROPERTIES_FILE}"
+
+
+"${JBOSS_HOME}"/bin/jboss-cli.sh --file="${JBOSS_HOME}/extensions/initialize-server.cli" --properties="${POSTCONFIGURE_PROPERTIES_FILE}"
